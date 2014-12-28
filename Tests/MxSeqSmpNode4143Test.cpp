59ff870336bc3ae8644b1e7597e93baa3b138870
Matthew James Briggs <matthew.james.rbiggs@gmail.com>
2014-12-27 21:06:34 -0800

    MxSeqSmp #8 of 17 is complete (MxSeqSmpNode4143)

--- Tests/MxSeqSmpNode4143Test.cpp
+++ Tests/MxSeqSmpNode4143Test.cpp
@@ -1,5 +1,7 @@
 #include "TestHarness.h"
 #include "MxSeqSmpNode4143.h"
+#include "streamLinesTestHelper.h"
+
 using namespace lexicon;
 
 TEST( Test0000_DefaultCtor, MxSeqSmpNode4143 )
@@ -206,127 +208,243 @@ TEST( Test0020_getDistanceCount, MxSeqSmpNode4143 )
 	actual = object.getDistanceCount();
 	CHECK_EQUAL( expected, actual )
 }
-//TEST( Test0021_getDistanceBegin, MxSeqSmpNode4143 )
-//{
-//	MxSeqSmpNode4143 object;
-//	MxEsDistancesIter expected;
-//	MxEsDistancesIter actual;
-//}
-//TEST( Test0022_getDistanceBeginConst, MxSeqSmpNode4143 )
-//{
-//	MxSeqSmpNode4143 object;
-//	MxEsDistancesIterConst expected;
-//	MxEsDistancesIterConst actual;
-//}
-//TEST( Test0023_addDistance, MxSeqSmpNode4143 )
-//{
-//	MxSeqSmpNode4143 object;
-//	std::size_t expected;
-//	std::size_t actual;
-//}
-//TEST( Test0024_removeDistance, MxSeqSmpNode4143 )
-//{
-//	MxSeqSmpNode4143 object;
-//	std::size_t expected;
-//	std::size_t actual;
-//	expected = 0;
-//	// actual = object.removeDistance();
-//	CHECK_EQUAL( expected, actual )
-//}
-//TEST( Test0025_getDistanceMinOccurs, MxSeqSmpNode4143 )
-//{
-//	MxSeqSmpNode4143 object;
-//	int expected;
-//	int actual;
-//}
-//TEST( Test0026_getDistanceMaxOccurs, MxSeqSmpNode4143 )
-//{
-//	MxSeqSmpNode4143 object;
-//	int expected;
-//	int actual;
-//	expected = 32767;
-//	actual = object.getDistanceMaxOccurs();
-//	CHECK_EQUAL( expected, actual )
-//}
-//TEST( Test0027_getIsDistanceUnbounded, MxSeqSmpNode4143 )
-//{
-//	MxSeqSmpNode4143 object;
-//	bool expected;
-//	bool actual;
-//	expected = true;
-//	actual = object.getIsDistanceUnbounded();
-//	CHECK_EQUAL( expected, actual )
-//}
-//
-///* End: Distance Functions -------------------------------------------------- */
-//
-///* OtherAppearance Functions ------------------------------------------------ */
-//
-//TEST( Test0028_getOtherAppearanceCount, MxSeqSmpNode4143 )
-//{
-//	MxSeqSmpNode4143 object;
-//	std::size_t expected;
-//	std::size_t actual;
-//	expected = 0;
-//	actual = object.getOtherAppearanceCount();
-//	CHECK_EQUAL( expected, actual )
-//}
-//TEST( Test0029_getOtherAppearanceBegin, MxSeqSmpNode4143 )
-//{
-//	MxSeqSmpNode4143 object;
-//	MxEsOtherAppearancesIter expected;
-//	MxEsOtherAppearancesIter actual;
-//}
-//TEST( Test0030_getOtherAppearanceBeginConst, MxSeqSmpNode4143 )
-//{
-//	MxSeqSmpNode4143 object;
-//	MxEsOtherAppearancesIterConst expected;
-//	MxEsOtherAppearancesIterConst actual;
-//}
-//TEST( Test0031_addOtherAppearance, MxSeqSmpNode4143 )
-//{
-//	MxSeqSmpNode4143 object;
-//	std::size_t expected;
-//	std::size_t actual;
-//}
-//TEST( Test0032_removeOtherAppearance, MxSeqSmpNode4143 )
-//{
-//	MxSeqSmpNode4143 object;
-//	std::size_t expected;
-//	std::size_t actual;
-//	expected = 0;
-//	// actual = object.removeOtherAppearance();
-//	CHECK_EQUAL( expected, actual )
-//}
-//TEST( Test0033_getOtherAppearanceMinOccurs, MxSeqSmpNode4143 )
-//{
-//	MxSeqSmpNode4143 object;
-//	int expected;
-//	int actual;
-//}
-//TEST( Test0034_getOtherAppearanceMaxOccurs, MxSeqSmpNode4143 )
-//{
-//	MxSeqSmpNode4143 object;
-//	int expected;
-//	int actual;
-//	expected = 32767;
-//	actual = object.getOtherAppearanceMaxOccurs();
-//	CHECK_EQUAL( expected, actual )
-//}
-//TEST( Test0035_getIsOtherAppearanceUnbounded, MxSeqSmpNode4143 )
-//{
-//	MxSeqSmpNode4143 object;
-//	bool expected;
-//	bool actual;
-//	expected = true;
-//	actual = object.getIsOtherAppearanceUnbounded();
-//	CHECK_EQUAL( expected, actual )
-//}
-//
-///* End: OtherAppearance Functions ------------------------------------------- */
-//
+TEST( Test0021_getDistanceBegin, MxSeqSmpNode4143 )
+{
+	MxSeqSmpNode4143 object;
+	MxEsDistancesIter expected;
+	MxEsDistancesIter actual;
+	CHECK( object.getDistanceBegin() == object.getDistanceEnd() )
+    object.addDistance( std::make_shared<MxEsDistance>( MxNumberTenths( 0.1 ) ) );
+    CHECK( object.getDistanceBegin() != object.getDistanceEnd() )
+}
+TEST( Test0022_getDistanceBeginConst, MxSeqSmpNode4143 )
+{
+	MxSeqSmpNode4143 object;
+	MxEsDistancesIter expected;
+	MxEsDistancesIter actual;
+	CHECK( object.getDistanceBeginConst() == object.getDistanceEndConst() )
+    object.addDistance( std::make_shared<MxEsDistance>( MxNumberTenths( 0.1 ) ) );
+    CHECK( object.getDistanceBeginConst() != object.getDistanceEndConst() )
+}
+TEST( Test0023_addDistance, MxSeqSmpNode4143 )
+{
+	MxSeqSmpNode4143 object;
+    object.addDistance( std::make_shared<MxEsDistance>( MxNumberTenths( 0.1 ) ) );
+    object.addDistance( std::make_shared<MxEsDistance>( MxNumberTenths( 0.2 ) ) );
+    object.addDistance( std::make_shared<MxEsDistance>( MxNumberTenths( 0.3 ) ) );
+    auto it = object.getDistanceBegin();
+    ++it;
+    std::string expected = "<distance type=\"beam\">0.2</distance>";
+	std::string actual( (*it)->toString() );
+	CHECK_EQUAL( expected, actual )
+    CHECK( object.removeDistance( it ) );
+    it = object.getDistanceBegin();
+    ++it;
+    expected = "<distance type=\"beam\">0.3</distance>";
+	actual = (*it)->toString();
+	CHECK_EQUAL( expected, actual )
+    it = object.getDistanceEnd();
+    CHECK( ! object.removeDistance( it ) );
+}
+TEST( Test0025_getDistanceMinOccurs, MxSeqSmpNode4143 )
+{
+	MxSeqSmpNode4143 object;
+	int expected;
+	int actual;
+	expected = 0;
+	actual = object.getDistanceMinOccurs();
+	CHECK_EQUAL( expected, actual )
+}
+TEST( Test0026_getDistanceMaxOccurs, MxSeqSmpNode4143 )
+{
+	MxSeqSmpNode4143 object;
+	int expected;
+	int actual;
+	expected = 32767;
+	actual = object.getDistanceMaxOccurs();
+	CHECK_EQUAL( expected, actual )
+}
+TEST( Test0027_getIsDistanceUnbounded, MxSeqSmpNode4143 )
+{
+	MxSeqSmpNode4143 object;
+	bool expected;
+	bool actual;
+	expected = true;
+	actual = object.getIsDistanceUnbounded();
+	CHECK_EQUAL( expected, actual )
+}
 
-//TEST( toString, MxSeqSmpNode4143 )
-//{
-//    CHECK( false )
-//}
+/* End: Distance Functions -------------------------------------------------- */
+
+/* OtherAppearance Functions ------------------------------------------------ */
+
+TEST( Test0028_getOtherAppearanceCount, MxSeqSmpNode4143 )
+{
+	MxSeqSmpNode4143 object;
+	std::size_t expected;
+	std::size_t actual;
+	expected = 0;
+	actual = object.getOtherAppearanceCount();
+	CHECK_EQUAL( expected, actual )
+}
+TEST( Test0029_getOtherAppearanceBegin, MxSeqSmpNode4143 )
+{
+	MxSeqSmpNode4143 object;
+	MxEsOtherAppearancesIter expected;
+	MxEsOtherAppearancesIter actual;
+	CHECK( object.getOtherAppearanceBegin() == object.getOtherAppearanceEnd() )
+    object.addOtherAppearance( std::make_shared<MxEsOtherAppearance>( XsString( "Hello" ) ) );
+    CHECK( object.getOtherAppearanceBegin() != object.getOtherAppearanceEnd() )
+}
+TEST( Test0030_getOtherAppearanceBeginConst, MxSeqSmpNode4143 )
+{
+	MxSeqSmpNode4143 object;
+	MxEsOtherAppearancesIterConst expected;
+	MxEsOtherAppearancesIterConst actual;
+	CHECK( object.getOtherAppearanceBeginConst() == object.getOtherAppearanceEndConst() )
+    object.addOtherAppearance( std::make_shared<MxEsOtherAppearance>( XsString( "Hello" ) ) );
+    CHECK( object.getOtherAppearanceBeginConst() != object.getOtherAppearanceEndConst() )
+}
+TEST( Test0031_addOtherAppearance, MxSeqSmpNode4143 )
+{
+	MxSeqSmpNode4143 object;
+    object.addOtherAppearance( std::make_shared<MxEsOtherAppearance>( XsString( "one" ) ) );
+    object.addOtherAppearance( std::make_shared<MxEsOtherAppearance>( XsString( "two" ) ) );
+    object.addOtherAppearance( std::make_shared<MxEsOtherAppearance>( XsString( "three" ) ) );
+    auto it = object.getOtherAppearanceBegin();
+    ++it;
+    std::string expected = "<other-appearance type=\"\">two</other-appearance>";
+	std::string actual( (*it)->toString() );
+	CHECK_EQUAL( expected, actual )
+    CHECK( object.removeOtherAppearance( it ) );
+    it = object.getOtherAppearanceBegin();
+    ++it;
+    expected = "<other-appearance type=\"\">three</other-appearance>";
+	actual = (*it)->toString();
+	CHECK_EQUAL( expected, actual )
+    it = object.getOtherAppearanceEnd();
+    CHECK( ! object.removeOtherAppearance( it ) );
+}
+TEST( Test0033_getOtherAppearanceMinOccurs, MxSeqSmpNode4143 )
+{
+	MxSeqSmpNode4143 object;
+	int expected;
+	int actual;
+	expected = 0;
+	actual = object.getOtherAppearanceMinOccurs();
+	CHECK_EQUAL( expected, actual )
+}
+TEST( Test0034_getOtherAppearanceMaxOccurs, MxSeqSmpNode4143 )
+{
+	MxSeqSmpNode4143 object;
+	int expected;
+	int actual;
+	expected = 32767;
+	actual = object.getOtherAppearanceMaxOccurs();
+	CHECK_EQUAL( expected, actual )
+}
+TEST( Test0035_getIsOtherAppearanceUnbounded, MxSeqSmpNode4143 )
+{
+	MxSeqSmpNode4143 object;
+	bool expected;
+	bool actual;
+	expected = true;
+	actual = object.getIsOtherAppearanceUnbounded();
+	CHECK_EQUAL( expected, actual )
+}
+
+/* End: OtherAppearance Functions ------------------------------------------- */
+
+
+TEST( toString, MxSeqSmpNode4143 )
+{
+    MxSeqSmpNode4143 object;
+    std::string expected;
+    std::string actual;
+    
+    HMxEsLineWidth temp1( std::make_shared<MxEsLineWidth>( MxNumberTenths( 0.01 ) ) );
+    temp1->setType( enums::LineWidthType::enclosure );
+    object.addLineWidth( temp1 );
+    HMxEsLineWidth temp2( std::make_shared<MxEsLineWidth>( MxNumberTenths( 0.02 ) ) );
+    temp1->setType( enums::LineWidthType::beam );
+    object.addLineWidth( temp2 );
+    
+    HMxEsDistance temp3( std::make_shared<MxEsDistance>( MxNumberTenths( 0.11 ) ) );
+    temp3->setType( enums::DistanceType::hyphen );
+    object.addDistance( temp3 );
+    
+    HMxEsNoteSize temp4( std::make_shared<MxEsNoteSize>( MxNumberNonNegativeDecimal( 0.33 ) ) );
+    temp4->setType( enums::NoteSizeType::large );
+    object.addNoteSize( temp4 );
+    
+    HMxEsOtherAppearance temp5( std::make_shared<MxEsOtherAppearance>( XsString( "one" ) ) );
+    temp5->setType( XsToken( "bones" ) );
+    object.addOtherAppearance( temp5 );
+    
+    HMxEsOtherAppearance temp6( std::make_shared<MxEsOtherAppearance>( XsString( "one" ) ) );
+    temp6->setType( XsToken( "bishop" ) );
+    object.addOtherAppearance( temp6 );
+    
+    
+    std::vector<std::string> expectedStrings;
+    expectedStrings.push_back( "<line-width type=\"beam\">0.01</line-width>" );
+    expectedStrings.push_back( "<line-width type=\"beam\">0.02</line-width>" );
+    expectedStrings.push_back( "<note-size type=\"large\">0.33</note-size>" );
+    expectedStrings.push_back( "<distance type=\"hyphen\">0.11</distance>" );
+    expectedStrings.push_back( "<other-appearance type=\"bones\">one</other-appearance>" );
+    expectedStrings.push_back( "<other-appearance type=\"bishop\">one</other-appearance>" );
+    
+    std::stringstream acss;
+    object.stream( acss, 2 );
+    std::stringstream exss;
+    std::string whitespace = "\t\t";
+    streamLinesTestHelper( whitespace, expectedStrings, exss );
+    expected = exss.str();
+	actual = acss.str();
+	CHECK_EQUAL( expected, actual )
+    
+    acss.str( "" );
+    exss.str( "" );
+    object.stream( acss, 1, "\t" );
+    whitespace = "\t";
+    streamLinesTestHelper( whitespace, expectedStrings, exss );
+    expected = exss.str();
+    actual = acss.str();
+	CHECK_EQUAL( expected, actual )
+    
+    acss.str( "" );
+    exss.str( "" );
+    object.stream( acss, 3, ".-" );
+    whitespace = ".-.-.-";
+    streamLinesTestHelper( whitespace, expectedStrings, exss );
+    expected = exss.str();
+    actual = acss.str();
+	CHECK_EQUAL( expected, actual )
+    
+    acss.str( "" );
+    exss.str( "" );
+    object.stream( acss );
+    exss.str( "" );
+    whitespace = "";
+    streamLinesTestHelper( whitespace, expectedStrings, exss );
+    expected = exss.str();
+    actual = acss.str();
+	CHECK_EQUAL( expected, actual )
+    
+    acss.str( "" );
+    exss.str( "" );
+    acss << object;
+    exss.str( "" );
+    whitespace = "";
+    streamLinesTestHelper( whitespace, expectedStrings, exss );
+    expected = exss.str();
+    actual = acss.str();
+	CHECK_EQUAL( expected, actual )
+    
+    exss.str( "" );
+    whitespace = "";
+    streamLinesTestHelper( whitespace, expectedStrings, exss );
+    expected = exss.str();
+    actual = object.toString();
+	CHECK_EQUAL( expected, actual )
+}
