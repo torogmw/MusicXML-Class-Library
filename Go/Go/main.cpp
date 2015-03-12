5da1b3be0e4cca354752cb71897b3479d535d946
Matthew James Briggs <matthew.james.rbiggs@gmail.com>
2015-03-12 16:27:02 -0700

    Thinking about aggregate elements

--- Go/Go/main.cpp
+++ Go/Go/main.cpp
@@ -3,15 +3,17 @@
 #include "goSetIsImplemented.h"
 #include "goMsItemAttributeTest.h"
 // #include "goCreateValueElements.h"
-#include "goCreateValueNoAttributeElements.h"
-
+// #include "goCreateValueNoAttributeElements.h"
+#include "goCreateOneSequenceElements.h"
 #include <iomanip>
+
 
 int main(int argc, const char * argv[])
 {
     // go::goSetIsImplemented( std::vector<int>{ 2517, 5114 } );
     // go::goMsItemAttributeTest();
     // go::goCreateValueElements();
-    go::goCreateValueNoAttributeElementsImplementOne();
+    // go::goCreateValueNoAttributeElementsImplementOne();
+    go::goSDKJGFHSDLFJKGH();
     return 0;
 }
