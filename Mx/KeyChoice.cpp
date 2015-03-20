f83209f01b974de253a57beb9176cf290ce466b9
Matthew James Briggs <matthew.james.rbiggs@gmail.com>
2015-03-20 14:19:44 -0700

    c

--- Mx/KeyChoice.cpp
+++ Mx/KeyChoice.cpp
@@ -4,6 +4,39 @@ namespace mx
 {
     namespace e
     {
-        
+        bool KeyChoice::hasAttributes() const
+        {
+            return false;
+        }
+        std::ostream& KeyChoice::streamAttributes( std::ostream& os ) const
+        {
+            return os;
+        }
+        std::ostream& KeyChoice::streamName( std::ostream& os ) const
+        {
+            return os;
+        }
+        bool KeyChoice::hasContents() const
+        {
+            return true;
+        }
+        std::ostream& KeyChoice::streamContents( std::ostream& os, const int indentLevel, bool& isOneLineOnly ) const
+        {
+            if ( myChoice == Choice::traditionalKey )
+            {
+                if ( myTraditionalKey )
+                {
+                    myTraditionalKey->streamContents( os, indentLevel, isOneLineOnly );
+                }
+            }
+            else if ( myChoice == Choice::nonTraditionalKey )
+            {
+                if ( myNonTraditionalKey )
+                {
+                    myNonTraditionalKey->streamContents( os, indentLevel, isOneLineOnly );
+                }
+            }
+            return os;
+        }
     }
 }
