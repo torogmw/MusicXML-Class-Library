0513275ca10608804173165477f2a6761d45384b
Matthew James Briggs <matthew.james.rbiggs@gmail.com>
2015-03-24 12:48:48 -0700

    save

--- Mx/ElementsNew.cpp
+++ Mx/ElementsNew.cpp
@@ -4,6 +4,240 @@ namespace mx
 {
     namespace e
     {
+        
+        
+        /**************** StaffDetailsAttributes ****************/
+        /* 2653 */
+        StaffDetailsAttributes::StaffDetailsAttributes()
+        :number()
+        ,showFrets()
+        ,printObject( types::YesNo::no )
+        ,printSpacing( types::YesNo::no )
+        ,hasNumber( false )
+        ,hasShowFrets( false )
+        ,hasPrintObject( false )
+        ,hasPrintSpacing( false )
+        {}
+        
+        bool StaffDetailsAttributes::hasValues() const
+        {
+            return hasNumber ||
+            hasShowFrets ||
+            hasPrintObject ||
+            hasPrintSpacing;
+        }
+        
+        std::ostream& StaffDetailsAttributes::toStream( std::ostream& os ) const
+        {
+            if ( hasValues() )
+            {
+                streamAttribute( os, number, "number", hasNumber );
+                streamAttribute( os, showFrets, "show-frets", hasShowFrets );
+                streamAttribute( os, printObject, "print-object", hasPrintObject );
+                streamAttribute( os, printSpacing, "print-spacing", hasPrintSpacing );
+            }
+            return os;
+        }
+        
+		StaffDetails::StaffDetails()
+		:myAttributes( std::make_shared<StaffDetailsAttributes>() )
+		,myStaffType( makeStaffType() )
+		,myHasStaffType( false )
+		,myStaffLines( makeStaffLines() )
+		,myHasStaffLines( false )
+		,myStaffTuningSet()
+		,myTuningStep( makeTuningStep() )
+		,myTuningAlter( makeTuningAlter() )
+		,myHasTuningAlter( false )
+		,myTuningOctave( makeTuningOctave() )
+		,myCapo( makeCapo() )
+		,myHasCapo( false )
+		,myStaffSize( makeStaffSize() )
+		,myHasStaffSize( false )
+		{}
+		bool StaffDetails::hasAttributes() const
+		{
+			return myAttributes->hasValues();
+		}
+		std::ostream& StaffDetails::streamAttributes( std::ostream& os ) const
+		{
+			return myAttributes->toStream( os );
+			return os;
+		}
+		std::ostream& StaffDetails::streamName( std::ostream& os ) const
+		{
+			os << "staff-details";
+			return os;
+		}
+		bool StaffDetails::hasContents() const
+		{
+			return true;
+		}
+		std::ostream& StaffDetails::streamContents( std::ostream& os, const int indentLevel, bool& isOneLineOnly ) const
+		{
+			isOneLineOnly = false;
+			os << std::endl;
+			// mySign->toStream( os, indentLevel+1 );
+			throw std::runtime_error{ "not implemented" };
+		}
+		StaffDetailsAttributesPtr StaffDetails::getAttributes() const
+		{
+			return myAttributes;
+		}
+		void StaffDetails::setAttributes( const StaffDetailsAttributesPtr& value )
+		{
+			if ( value )
+			{
+				myAttributes = value;
+			}
+		}
+		/* _________ StaffType minOccurs = 0, maxOccurs = 1 _________ */
+		StaffTypePtr StaffDetails::getStaffType() const
+		{
+			return myStaffType;
+		}
+		void StaffDetails::setStaffType( const StaffTypePtr& value )
+		{
+			if( value )
+			{
+				myStaffType = value;
+			}
+		}
+		bool StaffDetails::getHasStaffType() const
+		{
+			return myHasStaffType;
+		}
+		void StaffDetails::setHasStaffType( const bool value )
+		{
+			myHasStaffType = value;
+		}
+		/* _________ StaffLines minOccurs = 0, maxOccurs = 1 _________ */
+		StaffLinesPtr StaffDetails::getStaffLines() const
+		{
+			return myStaffLines;
+		}
+		void StaffDetails::setStaffLines( const StaffLinesPtr& value )
+		{
+			if( value )
+			{
+				myStaffLines = value;
+			}
+		}
+		bool StaffDetails::getHasStaffLines() const
+		{
+			return myHasStaffLines;
+		}
+		void StaffDetails::setHasStaffLines( const bool value )
+		{
+			myHasStaffLines = value;
+		}
+		/* _________ StaffTuning minOccurs = 0, maxOccurs = unbounded _________ */
+		const StaffTuningSet& StaffDetails::getStaffTuningSet() const
+		{
+			return myStaffTuningSet;
+		}
+		void StaffDetails::removeStaffTuning( const StaffTuningSetIterConst& value )
+		{
+			if ( value != myStaffTuningSet.cend() )
+			{
+				myStaffTuningSet.erase( value );
+			}
+		}
+		void StaffDetails::addStaffTuning( const StaffTuningPtr& value )
+		{
+			if ( value )
+			{
+				myStaffTuningSet.push_back( value );
+			}
+		}
+		void StaffDetails::clearStaffTuningSet()
+		{
+			myStaffTuningSet.clear();
+		}
+		/* _________ TuningStep minOccurs = 1, maxOccurs = 1 _________ */
+		TuningStepPtr StaffDetails::getTuningStep() const
+		{
+			return myTuningStep;
+		}
+		void StaffDetails::setTuningStep( const TuningStepPtr& value )
+		{
+			if( value )
+			{
+				myTuningStep = value;
+			}
+		}
+		/* _________ TuningAlter minOccurs = 0, maxOccurs = 1 _________ */
+		TuningAlterPtr StaffDetails::getTuningAlter() const
+		{
+			return myTuningAlter;
+		}
+		void StaffDetails::setTuningAlter( const TuningAlterPtr& value )
+		{
+			if( value )
+			{
+				myTuningAlter = value;
+			}
+		}
+		bool StaffDetails::getHasTuningAlter() const
+		{
+			return myHasTuningAlter;
+		}
+		void StaffDetails::setHasTuningAlter( const bool value )
+		{
+			myHasTuningAlter = value;
+		}
+		/* _________ TuningOctave minOccurs = 1, maxOccurs = 1 _________ */
+		TuningOctavePtr StaffDetails::getTuningOctave() const
+		{
+			return myTuningOctave;
+		}
+		void StaffDetails::setTuningOctave( const TuningOctavePtr& value )
+		{
+			if( value )
+			{
+				myTuningOctave = value;
+			}
+		}
+		/* _________ Capo minOccurs = 0, maxOccurs = 1 _________ */
+		CapoPtr StaffDetails::getCapo() const
+		{
+			return myCapo;
+		}
+		void StaffDetails::setCapo( const CapoPtr& value )
+		{
+			if( value )
+			{
+				myCapo = value;
+			}
+		}
+		bool StaffDetails::getHasCapo() const
+		{
+			return myHasCapo;
+		}
+		void StaffDetails::setHasCapo( const bool value )
+		{
+			myHasCapo = value;
+		}
+		/* _________ StaffSize minOccurs = 0, maxOccurs = 1 _________ */
+		StaffSizePtr StaffDetails::getStaffSize() const
+		{
+			return myStaffSize;
+		}
+		void StaffDetails::setStaffSize( const StaffSizePtr& value )
+		{
+			if( value )
+			{
+				myStaffSize = value;
+			}
+		}
+		bool StaffDetails::getHasStaffSize() const
+		{
+			return myHasStaffSize;
+		}
+		void StaffDetails::setHasStaffSize( const bool value )
+		{
+			myHasStaffSize = value;
+		}
 
     }
 }
