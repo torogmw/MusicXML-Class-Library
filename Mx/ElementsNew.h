1b98518a2ee8ee579497b8805b57db0f34e8b9df
Matthew James Briggs <matthew.james.rbiggs@gmail.com>
2015-03-24 13:37:06 -0700

    save

--- Mx/ElementsNew.h
+++ Mx/ElementsNew.h
@@ -132,6 +132,125 @@ namespace mx
             StaffSizePtr myStaffSize;
             bool myHasStaffSize;
         };
-
+        /*
+         2681
+         <!--  ID = 2681 [2681] ------------------------->
+         <!-- min=0 max=4294967295 ZeroOrMany  -->
+         <!-- RecursiveSubElementCount = 8 -->
+         <xs:element name="measure-style" type="measure-style" minOccurs="0" maxOccurs="unbounded">
+         <xs:annotation>
+         <xs:documentation>A measure-style indicates a special way to print partial to multiple measures within a part. This includes multiple rests over several measures, repeats of beats, single, or multiple measures, and use of slash notation.</xs:documentation>
+         </xs:annotation>
+         </xs:element>
+         <xs:complexType name="measure-style">
+         <xs:annotation>
+         <xs:documentation>A measure-style indicates a special way to print partial to multiple measures within a part. This includes multiple rests over several measures, repeats of beats, single, or multiple measures, and use of slash notation.
+         
+         The multiple-rest and measure-repeat symbols indicate the number of measures covered in the element content. The beat-repeat and slash elements can cover partial measures. All but the multiple-rest element use a type attribute to indicate starting and stopping the use of the style. The optional number attribute specifies the staff number from top to bottom on the system, as with clef.</xs:documentation>
+         </xs:annotation>
+         <xs:choice>
+         <xs:element name="multiple-rest" type="multiple-rest"/>
+         <xs:element name="measure-repeat" type="measure-repeat"/>
+         <xs:element name="beat-repeat" type="beat-repeat"/>
+         <xs:element name="slash" type="slash"/>
+         </xs:choice>
+         <xs:attribute name="number" type="staff-number"/>
+         <xs:attributeGroup ref="font"/>
+         <xs:attributeGroup ref="color"/>
+         </xs:complexType>         */
+        
+        class MeasureStyleChoice;
+        using MeasureStyleChoicePtr = std::shared_ptr<MeasureStyleChoice>;
+        using MeasureStyleChoiceUPtr = std::unique_ptr<MeasureStyleChoice>;
+        using MeasureStyleChoiceSet = std::vector<MeasureStyleChoicePtr>;
+        using MeasureStyleChoiceSetIter = MeasureStyleSet::iterator;
+        using MeasureStyleChoiceSetIterConst = MeasureStyleSet::const_iterator;
+        inline MeasureStylePtr makeMeasureStyleChoice() { return std::make_shared<MeasureStyleChoice>(); }
+        class MeasureStyle : public ElementInterface
+        
+        struct MeasureStyleAttributes;
+        using MeasureStyleAttributesPtr = std::shared_ptr<MeasureStyleAttributes>;
+        
+        struct MeasureStyleAttributes : public AttributesInterface
+        {
+        public:
+            MeasureStyleAttributes();
+            virtual bool hasValues() const;
+            virtual std::ostream& toStream( std::ostream& os ) const;
+            types::StaffNumber number;
+            types::CommaSeparatedText fontFamily;
+            types::FontStyle fontStyle;
+            types::FontSize fontSize;
+            types::FontWeight fontWeight;
+            types::Color color;
+            bool hasNumber;
+            bool hasFontFamily;
+            bool hasFontStyle;
+            bool hasFontSize;
+            bool hasFontWeight;
+            bool hasColor;
+        };
+        
+        class MeasureStyle;
+        using MeasureStylePtr = std::shared_ptr<MeasureStyle>;
+        using MeasureStyleUPtr = std::unique_ptr<MeasureStyle>;
+        using MeasureStyleSet = std::vector<MeasureStylePtr>;
+        using MeasureStyleSetIter = MeasureStyleSet::iterator;
+        using MeasureStyleSetIterConst = MeasureStyleSet::const_iterator;
+        inline MeasureStylePtr makeMeasureStyle() { return std::make_shared<MeasureStyle>(); }
+        class MeasureStyle : public ElementInterface
+        {
+        public:
+            MeasureStyle();
+            virtual bool hasAttributes() const;
+            virtual std::ostream& streamAttributes( std::ostream& os ) const;
+            virtual std::ostream& streamName( std::ostream& os ) const;
+            virtual bool hasContents() const;
+            virtual std::ostream& streamContents( std::ostream& os, const int indentLevel, bool& isOneLineOnly ) const;
+            MeasureStyleAttributesPtr getAttributes() const;
+            void setAttributes( const MeasureStyleAttributesPtr& value );
+            /* _________ MultipleRest minOccurs = 1, maxOccurs = 1 _________ */
+            MultipleRestPtr getMultipleRest() const;
+            void setMultipleRest( const MultipleRestPtr& value );
+            /* _________ MeasureRepeat minOccurs = 1, maxOccurs = 1 _________ */
+            MeasureRepeatPtr getMeasureRepeat() const;
+            void setMeasureRepeat( const MeasureRepeatPtr& value );
+            /* _________ BeatRepeat minOccurs = 1, maxOccurs = 1 _________ */
+            BeatRepeatPtr getBeatRepeat() const;
+            void setBeatRepeat( const BeatRepeatPtr& value );
+            /* _________ SlashType minOccurs = 1, maxOccurs = 1 _________ */
+            SlashTypePtr getSlashType() const;
+            void setSlashType( const SlashTypePtr& value );
+            /* _________ SlashDot minOccurs = 0, maxOccurs = unbounded _________ */
+            const SlashDotSet& getSlashDotSet() const;
+            void addSlashDot( const SlashDotPtr& value );
+            void removeSlashDot( const SlashDotSetIterConst& value );
+            bool getHasSlashDot() const;
+            void setHasSlashDot( const bool value );
+            void clearSlashDotSet();
+            /* _________ Slash minOccurs = 1, maxOccurs = 1 _________ */
+            SlashPtr getSlash() const;
+            void setSlash( const SlashPtr& value );
+            /* _________ SlashType minOccurs = 1, maxOccurs = 1 _________ */
+            SlashTypePtr getSlashType() const;
+            void setSlashType( const SlashTypePtr& value );
+            /* _________ SlashDot minOccurs = 0, maxOccurs = unbounded _________ */
+            const SlashDotSet& getSlashDotSet() const;
+            void addSlashDot( const SlashDotPtr& value );
+            void removeSlashDot( const SlashDotSetIterConst& value );
+            bool getHasSlashDot() const;
+            void setHasSlashDot( const bool value );
+            void clearSlashDotSet();
+        private:
+            MeasureStyleAttributesPtr myAttributes;
+            MultipleRestPtr myMultipleRest;
+            MeasureRepeatPtr myMeasureRepeat;
+            BeatRepeatPtr myBeatRepeat;
+            SlashTypePtr mySlashType;
+            SlashDotSet mySlashDotSet;
+            SlashPtr mySlash;
+            SlashTypePtr mySlashType;
+            SlashDotSet mySlashDotSet;
+        };
     }
 }
