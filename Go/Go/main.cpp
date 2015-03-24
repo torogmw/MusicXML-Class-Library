7e63d1479ca877722f1dfb9e9b6b0be8a6959900
Matthew James Briggs <matthew.james.rbiggs@gmail.com>
2015-03-24 11:24:01 -0700

    save

--- Go/Go/main.cpp
+++ Go/Go/main.cpp
@@ -6,8 +6,8 @@
 
 int main(int argc, const char * argv[])
 {
-    // go::goSetIsImplemented( std::vector<int>{ 2839 } );
-    // go::goListNonImplementedElements();
+    go::goSetIsImplemented( std::vector<int>{ 2908 } );
+    go::goListNonImplementedElements();
     go::goCreateElementStubs( 2908 );
     return 0;
 }
