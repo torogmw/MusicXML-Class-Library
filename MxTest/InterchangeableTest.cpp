0c90f3691f3c97c4991ed19b39853ecfc490081c
Matthew James Briggs <matthew.james.rbiggs@gmail.com>
2015-03-24 14:44:01 -0700

    save

--- MxTest/InterchangeableTest.cpp
+++ MxTest/InterchangeableTest.cpp
@@ -11,14 +11,13 @@ TEST( Test01, Interchangeable )
 {
 	Interchangeable object;
 	stringstream expected;
-	streamLine( expected, 1, R"()" );
-	streamLine( expected, 2, R"()" );
-	streamLine( expected, 2, R"()" );
-	streamLine( expected, 2, R"()" );
-	streamLine( expected, 1, R"()", false );
+	streamLine( expected, 1, R"(<interchangeable>)" );
+	streamLine( expected, 2, R"(<beats></beats>)" );
+	streamLine( expected, 2, R"(<beat-type></beat-type>)" );
+	streamLine( expected, 1, R"(</interchangeable>)", false );
 	stringstream actual;
-	object.toStream( std::cout, 1 );
-	// object.toStream( actual, 1 );
+	// object.toStream( std::cout, 1 );
+	object.toStream( actual, 1 );
 	CHECK_EQUAL( expected.str(), actual.str() )
 	CHECK( ! object.hasAttributes() )
 	CHECK( object.hasContents() )
