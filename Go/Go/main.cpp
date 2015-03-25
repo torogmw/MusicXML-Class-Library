aeaff3046af0bf27cd63f50c245ab5f6c967430d
Matthew James Briggs <matthew.james.rbiggs@gmail.com>
2015-03-24 23:31:41 -0700

    HarpPedals implemented, 84 to go

--- Go/Go/main.cpp
+++ Go/Go/main.cpp
@@ -6,8 +6,8 @@
 
 int main(int argc, const char * argv[])
 {
-    go::goSetIsImplemented( std::vector<int>{ 3554 } );
-    go::goListNonImplementedElements();
-    // go::goCreateElementStubs( 3373 );
+    // go::goSetIsImplemented( std::vector<int>{ 3373 } );
+    // go::goListNonImplementedElements();
+    go::goCreateElementStubs( 3682 );
     return 0;
 }
