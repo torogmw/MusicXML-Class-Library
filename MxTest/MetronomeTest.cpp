8d6eb6af2b6e088b3add9392152069618ddb0691
Matthew James Briggs <matthew.james.rbiggs@gmail.com>
2015-03-25 16:47:37 -0700

    save

---
+++ MxTest/MetronomeTest.cpp
@@ -0,0 +1,41 @@
+#include "TestHarness.h"
+#include "MxTestHelper.h"
+#include "ElementsNew.h"
+
+using namespace mx::e;
+using namespace mx::types;
+using namespace std;
+using namespace MxTestHelpers;
+
+TEST( Test01, Metronome )
+{
+	Metronome object;
+	stringstream expected;
+	streamLine( expected, 1, R"()" );
+	streamLine( expected, 2, R"()" );
+	streamLine( expected, 2, R"()" );
+	streamLine( expected, 2, R"()" );
+	streamLine( expected, 1, R"()", false );
+	stringstream actual;
+	object.toStream( std::cout, 1 );
+	// object.toStream( actual, 1 );
+	CHECK_EQUAL( expected.str(), actual.str() )
+	CHECK( ! object.hasAttributes() )
+	CHECK( object.hasContents() )
+}
+TEST( Test02, Metronome )
+{
+	Metronome object;
+	stringstream expected;
+	streamLine( expected, 1, R"()" );
+	streamLine( expected, 2, R"()" );
+	streamLine( expected, 2, R"()" );
+	streamLine( expected, 2, R"()" );
+	streamLine( expected, 1, R"()", false );
+	stringstream actual;
+	object.toStream( std::cout, 1 );
+	// object.toStream( actual, 1 );
+	CHECK_EQUAL( expected.str(), actual.str() )
+	CHECK( object.hasAttributes() )
+	CHECK( object.hasContents() )
+}
