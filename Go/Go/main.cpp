a7d43f93aad496f28d1a02e236791fdf6712ef8f
Matthew James Briggs <matthew.james.rbiggs@gmail.com>
2015-03-26 14:17:46 -0700

    Accord element implemented, 73 to go

--- Go/Go/main.cpp
+++ Go/Go/main.cpp
@@ -6,8 +6,8 @@
 
 int main(int argc, const char * argv[])
 {
-    // go::goSetIsImplemented( std::vector<int>{ 3861 } );
-    // go::goListNonImplementedElements();
-    go::goCreateElementStubs( 3910 );
+    go::goSetIsImplemented( std::vector<int>{ 3910 } );
+    go::goListNonImplementedElements();
+    go::goCreateElementStubs( 3926 );
     return 0;
 }
--- README.md
+++ README.md
@@ -3,4 +3,4 @@ MX DOM
 
 C++ binding for MusicXML.  This is a work in progress.
 
-Current Status as of 2015-03-26, 347 Elements Implemented, 74 Elements to go.
+Current Status as of 2015-03-26, 348 Elements Implemented, 73 Elements to go.
