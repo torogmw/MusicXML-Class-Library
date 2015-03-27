03fa314c7dd5f18881ecb8726b09ba4b4b181495
Matthew James Briggs <matthew.james.rbiggs@gmail.com>
2015-03-26 21:31:38 -0700

    MidiInstrument element implemented, 70 to go

--- Go/Go/main.cpp
+++ Go/Go/main.cpp
@@ -6,8 +6,8 @@
 
 int main(int argc, const char * argv[])
 {
-    go::goSetIsImplemented( std::vector<int>{ 3301 } );
-    go::goListNonImplementedElements();
+    // go::goSetIsImplemented( std::vector<int>{ 3926 } );
+    // go::goListNonImplementedElements();
     go::goCreateElementStubs( 3926 );
     return 0;
 }
--- MxTest/MidiInstrumentTest.cpp
+++ MxTest/MidiInstrumentTest.cpp
@@ -20,7 +20,7 @@ TEST( Test01, MidiInstrument )
 	object->toStream( actual, 1 );
 	CHECK_EQUAL( expected.str(), actual.str() )
 	CHECK( object->hasAttributes() )
-	CHECK( object->hasContents() )
+	CHECK( ! object->hasContents() )
 }
 TEST( Test02, MidiInstrument )
 {
@@ -32,7 +32,7 @@ TEST( Test02, MidiInstrument )
 	// object->toStream( std::cout, 1 );
 	object->toStream( actual, 1 );
 	CHECK_EQUAL( expected.str(), actual.str() )
-	CHECK( ! object->hasAttributes() )
+	CHECK( object->hasAttributes() )
 	CHECK( object->hasContents() )
 }
 TEST( Test03, MidiInstrument )
@@ -45,7 +45,7 @@ TEST( Test03, MidiInstrument )
 	// object->toStream( std::cout, 1 );
 	object->toStream( actual, 1 );
 	CHECK_EQUAL( expected.str(), actual.str() )
-	CHECK( ! object->hasAttributes() )
+	CHECK( object->hasAttributes() )
 	CHECK( object->hasContents() )
 }
 namespace MxTestHelpers
@@ -64,12 +64,38 @@ namespace MxTestHelpers
             {
                 o->getAttributes()->id = XsIDREF( "M1" );
                 o->setHasMidiChannel( true );
-                
+                o->setHasMidiName( true );
+                o->setHasMidiBank( true );
+                o->setHasMidiUnpitched( false );
+                o->setHasVolume( true );
+                o->setHasPan( true );
+                o->setHasElevation( false );
+                o->getMidiChannel()->setValue( Midi16( 2 ) );
+                o->getMidiName()->setValue( XsString( "Trumpet" ) );
+                o->getMidiBank()->setValue( Midi16384( 3 ) );
+                o->getMidiUnpitched()->setValue( Midi128( 100 ) );
+                o->getVolume()->setValue( Percent( 55.5 ) );
+                o->getPan()->setValue( RotationDegrees( -88.6 ) );
+                o->getElevation()->setValue( RotationDegrees( 101.112 ) );
             }
                 break;
             case variant::three:
             {
-
+                o->getAttributes()->id = XsIDREF( "X2" );
+                o->setHasMidiChannel( true );
+                o->setHasMidiName( true );
+                o->setHasMidiBank( true );
+                o->setHasMidiUnpitched( true );
+                o->setHasVolume( true );
+                o->setHasPan( false );
+                o->setHasElevation( true );
+                o->getMidiChannel()->setValue( Midi16( 3 ) );
+                o->getMidiName()->setValue( XsString( "Bassoon" ) );
+                o->getMidiBank()->setValue( Midi16384( 4 ) );
+                o->getMidiUnpitched()->setValue( Midi128( 97 ) );
+                o->getVolume()->setValue( Percent( 0.123 ) );
+                o->getPan()->setValue( RotationDegrees( 120.3 ) );
+                o->getElevation()->setValue( RotationDegrees( -33.333 ) );
             }
                 break;
             default:
@@ -89,16 +115,25 @@ namespace MxTestHelpers
                 break;
             case variant::two:
             {
-                streamLine( os, i, R"(<direction-type>)" );
-                streamLine( os, i+1, R"(<pedal type="start" line="yes"/>)" );
-                streamLine( os, i, R"(</direction-type>)", false );
+                streamLine( os, i, R"(<midi-instrument id="M1">)" );
+                streamLine( os, i+1, R"(<midi-channel>2</midi-channel>)" );
+                streamLine( os, i+1, R"(<midi-name>Trumpet</midi-name>)" );
+                streamLine( os, i+1, R"(<midi-bank>3</midi-bank>)" );
+                streamLine( os, i+1, R"(<volume>55.5</volume>)" );
+                streamLine( os, i+1, R"(<pan>-88.6</pan>)" );
+                streamLine( os, i, R"(</midi-instrument>)", false );
             }
                 break;
             case variant::three:
             {
-                streamLine( os, i, R"(<direction-type>)" );
-
-                streamLine( os, i, R"(</direction-type>)", false );
+                streamLine( os, i, R"(<midi-instrument id="X2">)" );
+                streamLine( os, i+1, R"(<midi-channel>3</midi-channel>)" );
+                streamLine( os, i+1, R"(<midi-name>Bassoon</midi-name>)" );
+                streamLine( os, i+1, R"(<midi-bank>4</midi-bank>)" );
+                streamLine( os, i+1, R"(<midi-unpitched>97</midi-unpitched>)" );
+                streamLine( os, i+1, R"(<volume>0.123</volume>)" );
+                streamLine( os, i+1, R"(<elevation>-33.333</elevation>)" );
+                streamLine( os, i, R"(</midi-instrument>)", false );
             }
                 break;
             default:
