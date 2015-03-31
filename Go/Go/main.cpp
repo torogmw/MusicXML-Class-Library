e056cfb3ea017886f73ddfdb770d2d30059696b2
Matthew James Briggs <matthew.james.rbiggs@gmail.com>
2015-03-30 22:01:14 -0700

    35.5 elements to go

--- Go/Go/main.cpp
+++ Go/Go/main.cpp
@@ -6,8 +6,8 @@
 
 int main(int argc, const char * argv[])
 {
-    // go::goSetIsImplemented( std::vector<int>{ 3926, 5959, 6191, 6206, 6210, 6233, 6237, 6241, 6290 } );
-    // go::goListNonImplementedElements();
-    go::goCreateElementStubs( 6290 );
+    go::goSetIsImplemented( std::vector<int>{ 3926, 5959, 6191, 6206, 6210, 6233, 6237, 6241, 6290, 6293,  } );
+    go::goListNonImplementedElements();
+    // go::goCreateElementStubs( -111 );
     return 0;
 }
--- README.md
+++ README.md
@@ -3,4 +3,4 @@ MX DOM
 
 C++ binding for MusicXML.  This is a work in progress.
 
-Current Status as of 2015-03-30, 382 Elements Implemented, 39 Elements to go.
+Current Status as of 2015-03-30, 386 Elements Implemented, 35 Elements to go.
