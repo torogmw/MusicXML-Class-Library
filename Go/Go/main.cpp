7a2f6c5fff71cd6b1af55aa37ad6a42794a42a79
Matthew James Briggs <matthew.james.rbiggs@gmail.com>
2015-04-02 16:02:09 -0700

    30 elements to go

--- Go/Go/main.cpp
+++ Go/Go/main.cpp
@@ -6,8 +6,8 @@
 
 int main(int argc, const char * argv[])
 {
-    // go::goSetIsImplemented( std::vector<int>{ 6296, 6310, 6313, 6325, 6334 } );
-    // go::goListNonImplementedElements();
-    go::goCreateElementStubs( 6334 );
+    go::goSetIsImplemented( std::vector<int>{ 6296, 6310, 6313, 6325, 6334 } );
+    go::goListNonImplementedElements();
+    // go::goCreateElementStubs( 6334 );
     return 0;
 }
