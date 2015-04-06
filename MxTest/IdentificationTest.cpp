ea7af6c6dd766db847fc5f4a85eedb5c797c7ddb
Matthew James Briggs <matthew.james.rbiggs@gmail.com>
2015-04-06 08:22:57 -0700

    Identification element implemented

--- MxTest/IdentificationTest.cpp
+++ MxTest/IdentificationTest.cpp
@@ -2,6 +2,8 @@
 #include "TestHarness.h"
 #include "MxTestHelper.h"
 #include "IdentificationTest.h"
+#include "MiscellaneousTest.h"
+#include "EncodingTest.h"
 
 using namespace mx::e;
 using namespace mx::types;
@@ -21,7 +23,7 @@ TEST( Test01, Identification )
 	object->toStream( actual, 1 );
 	CHECK_EQUAL( expected.str(), actual.str() )
 	CHECK( ! object->hasAttributes() )
-	CHECK( object->hasContents() )
+	CHECK( ! object->hasContents() )
 }
 TEST( Test02, Identification )
 {
@@ -33,7 +35,7 @@ TEST( Test02, Identification )
 	// object->toStream( std::cout, 1 );
 	object->toStream( actual, 1 );
 	CHECK_EQUAL( expected.str(), actual.str() )
-	CHECK( object->hasAttributes() )
+	CHECK( ! object->hasAttributes() )
 	CHECK( object->hasContents() )
 }
 TEST( Test03, Identification )
@@ -64,12 +66,50 @@ namespace MxTestHelpers
                 break;
             case variant::two:
             {
-
+                auto creator1 = makeCreator( XsString( "Matthew James Briggs" ) );
+                creator1->getAttributes()->hasType = true;
+                creator1->getAttributes()->type = XsToken( "composer" );
+                o->addCreator( creator1 );
+                auto creator2 = makeCreator( XsString( "Bones" ) );
+                creator2->getAttributes()->hasType = true;
+                creator2->getAttributes()->type = XsToken( "cat" );
+                o->addCreator( creator2 );
+                o->setHasMiscellaneous( true );
+                o->setMiscellaneous( tgenMiscellaneous( v ) );
+                o->setHasEncoding( true );
+                o->setEncoding( tgenEncoding( v ) );
+                auto rights1 = makeRights( XsString( "© 2015" ) );
+                rights1->getAttributes()->hasType = true;
+                rights1->getAttributes()->type = XsToken( "copyright" );
+                o->addRights( rights1 );
+                o->setHasSource( true );
+                o->getSource()->setValue( XsString( "My Brain" ) );
             }
                 break;
             case variant::three:
             {
-
+                auto creator1 = makeCreator( XsString( "world" ) );
+                creator1->getAttributes()->hasType = true;
+                creator1->getAttributes()->type = XsToken( "hello" );
+                o->addCreator( creator1 );
+                auto creator2 = makeCreator( XsString( "Bishop" ) );
+                creator2->getAttributes()->hasType = true;
+                creator2->getAttributes()->type = XsToken( "othercat" );
+                o->addCreator( creator2 );
+                o->setHasMiscellaneous( true );
+                o->setMiscellaneous( tgenMiscellaneous( v ) );
+                o->setHasEncoding( true );
+                o->setEncoding( tgenEncoding( v ) );
+                auto rights1 = makeRights( XsString( "unalienable" ) );
+                rights1->getAttributes()->hasType = true;
+                rights1->getAttributes()->type = XsToken( "human" );
+                o->addRights( rights1 );
+                auto rights2 = makeRights( XsString( "yes" ) );
+                rights2->getAttributes()->hasType = true;
+                rights2->getAttributes()->type = XsToken( "animal" );
+                o->addRights( rights2 );
+                o->setHasSource( true );
+                o->getSource()->setValue( XsString( "Manuscript" ) );
             }
                 break;
             default:
@@ -84,23 +124,36 @@ namespace MxTestHelpers
         {
             case variant::one:
             {
-                streamLine( os, i, R"(<group-abbreviation-display>)" );
-                streamLine( os, i+1, R"(<display-text></display-text>)" );
-                streamLine( os, i, R"(</group-abbreviation-display>)", false );
+                streamLine( os, i, R"(<identification/>)", false );
             }
                 break;
             case variant::two:
             {
-                streamLine( os, i, R"(<group-abbreviation-display print-object="no">)" );
-                streamLine( os, i+1, R"(<accidental-text>double-sharp</accidental-text>)" );
-                streamLine( os, i, R"(</group-abbreviation-display>)", false );
+                streamLine( os, i, R"(<identification>)" );
+                streamLine( os, i+1, R"(<creator type="composer">Matthew James Briggs</creator>)" );
+                streamLine( os, i+1, R"(<creator type="cat">Bones</creator>)" );
+                streamLine( os, i+1, R"(<rights type="copyright">© 2015</rights>)" );
+                tgenEncodingExpected( os, i+1, v );
+                os << std::endl;
+                streamLine( os, i+1, R"(<source>My Brain</source>)" );
+                tgenMiscellaneousExpected( os, i+1, v );
+                os << std::endl;
+                streamLine( os, i, R"(</identification>)", false );
             }
                 break;
             case variant::three:
             {
-                streamLine( os, i, R"(<group-abbreviation-display>)" );
-                streamLine( os, i+1, R"(<display-text xml:space="preserve">My Display String!</display-text>)" );
-                streamLine( os, i, R"(</group-abbreviation-display>)", false );
+                streamLine( os, i, R"(<identification>)" );
+                streamLine( os, i+1, R"(<creator type="hello">world</creator>)" );
+                streamLine( os, i+1, R"(<creator type="othercat">Bishop</creator>)" );
+                streamLine( os, i+1, R"(<rights type="human">unalienable</rights>)" );
+                streamLine( os, i+1, R"(<rights type="animal">yes</rights>)" );
+                tgenEncodingExpected( os, i+1, v );
+                os << std::endl;
+                streamLine( os, i+1, R"(<source>Manuscript</source>)" );
+                tgenMiscellaneousExpected( os, i+1, v );
+                os << std::endl;
+                streamLine( os, i, R"(</identification>)", false );
             }
                 break;
             default:
