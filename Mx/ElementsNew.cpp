8ade35362a0997dd004a53954221b1c7edf889df
Matthew James Briggs <matthew.james.rbiggs@gmail.com>
2015-04-18 15:04:03 -0700

    save

--- Mx/ElementsNew.cpp
+++ Mx/ElementsNew.cpp
@@ -2512,6 +2512,93 @@ namespace mx
                 myArtificial = value;
             }
         }
+        
+        HarmonicInfoChoice::HarmonicInfoChoice()
+        :myChoice( Choice::basePitch )
+        ,myBasePitch( makeBasePitch() )
+        ,myTouchingPitch( makeTouchingPitch() )
+        ,mySoundingPitch( makeSoundingPitch() )
+        {}
+        bool HarmonicInfoChoice::hasAttributes() const
+        {
+            return false;
+        }
+        std::ostream& HarmonicInfoChoice::streamAttributes( std::ostream& os ) const
+        {
+            return os;
+        }
+        std::ostream& HarmonicInfoChoice::streamName( std::ostream& os ) const
+        {
+            return os;
+        }
+        bool HarmonicInfoChoice::hasContents() const
+        {
+            return true;
+        }
+        std::ostream& HarmonicInfoChoice::streamContents( std::ostream& os, const int indentLevel, bool& isOneLineOnly ) const
+        {
+            switch ( myChoice )
+            {
+                case Choice::basePitch:
+                    myBasePitch->toStream( os, indentLevel );
+                    break;
+                case Choice::touchingPitch:
+                    myTouchingPitch->toStream( os, indentLevel );
+                    break;
+                case Choice::soundingPitch:
+                    mySoundingPitch->toStream( os, indentLevel );
+                    break;
+                default:
+                    break;
+            }
+            isOneLineOnly = false;
+            return os;
+        }
+        /* _________ Choice minOccurs = 1, maxOccurs = 1 _________ */
+        HarmonicInfoChoice::Choice HarmonicInfoChoice::getChoice() const
+        {
+            return myChoice;
+        }
+        void HarmonicInfoChoice::setChoice( const HarmonicInfoChoice::Choice value )
+        {
+            myChoice = value;
+        }
+        /* _________ BasePitch minOccurs = 1, maxOccurs = 1 _________ */
+        BasePitchPtr HarmonicInfoChoice::getBasePitch() const
+        {
+            return myBasePitch;
+        }
+        void HarmonicInfoChoice::setBasePitch( const BasePitchPtr& value )
+        {
+            if ( value )
+            {
+                myBasePitch = value;
+            }
+        }
+        /* _________ TouchingPitch minOccurs = 1, maxOccurs = 1 _________ */
+        TouchingPitchPtr HarmonicInfoChoice::getTouchingPitch() const
+        {
+            return myTouchingPitch;
+        }
+        void HarmonicInfoChoice::setTouchingPitch( const TouchingPitchPtr& value )
+        {
+            if ( value )
+            {
+                myTouchingPitch = value;
+            }
+        }
+        /* _________ SoundingPitch minOccurs = 1, maxOccurs = 1 _________ */
+        SoundingPitchPtr HarmonicInfoChoice::getSoundingPitch() const
+        {
+            return mySoundingPitch;
+        }
+        void HarmonicInfoChoice::setSoundingPitch( const SoundingPitchPtr& value )
+        {
+            if ( value )
+            {
+                mySoundingPitch = value;
+            }
+        }
 #if 1==0
 #endif
         
